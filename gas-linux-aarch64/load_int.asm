9605b9d7d73c68801ec259fe5e7a53258b857cf1
frank chen <frankchen@astri.org>
2024-01-12 08:56:43 +0000

    update load_int.asm

--- gas-linux-aarch64/load_int.asm
+++ gas-linux-aarch64/load_int.asm
@@ -1,5 +1,5 @@
 // Load 32-bit integer
-// aarch64-linux-gnu-as load_int.asm && aarch64-linux-gnu-ld load_int.o && ./a.out
+// aarch64-linux-gnu-as -o load_int.o load_int.asm && aarch64-linux-gnu-ld load_int.o && ./a.out
 // echo $?
 .data
     expected_value: .word  0x1a2b3c4d
@@ -8,6 +8,7 @@
     .global _start
 
 _start:
+    b fail_label
     // Load the 32-bit integer `0x1a2b3c4d` into the eax register
     mov x1, 0x3c4d              // Set the lower 8 bits
     movk x1, 0x1a2b, lsl 16
@@ -20,12 +21,12 @@ _start:
 
 success_label:
     // Exit the program
-    mov x0, 1              // exit code: 0
-    mov x8, 93             // syscall: exit
+    mov x0, #0              // exit code: 0
+    mov x8, #93             // syscall: exit
     svc 0
 
 fail_label:
     // Exit the program
-    mov x0, 1              // exit code: 0
-    mov x8, 93             // syscall: exit
+    mov x0, #1              // exit code: 0
+    mov x8, #93             // syscall: exit
     svc 0
